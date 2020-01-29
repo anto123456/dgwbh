4143cb058482ffa50a6b0b0622d7552a75360d48
anto123456 <57015879+anto123456@users.noreply.github.com>
2020-01-29 19:14:36 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -51,7 +51,7 @@ loop.close()
 
 @app.route('/')
 async def homepage(request):
-    html_file = path / 'view' / 'SignInPage.html'
+    html_file = path / 'view' / 'FileUpload.html'
     return HTMLResponse(html_file.open().read())
 
 
